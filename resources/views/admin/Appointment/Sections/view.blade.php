f8e6a741eee130f940829d0b9b118849265105cb
AbdullahMohammedAbdelmoemen <abdullah.mohammed1997@outlook.com>
2022-12-13 13:15:40 +0200

    change some label in tables

--- resources/views/admin/Appointment/Sections/view.blade.php
+++ resources/views/admin/Appointment/Sections/view.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.contentLayoutMaster')
 @php
-$breadcrumbs = [[['link' => route('appointments.sections.index'), 'name' => "اقسام المقابلات"], ['link' => route('appointments.sections.edit',$section), 'name' => "نعديل معلومات القسم : $section->section_name"]],['title'=> 'مشاهدة معلومات القسم']];
+$breadcrumbs = [[['link' => route('appointments.sections.index'), 'name' => "اقسام المقابلات"], ['link' => route('appointments.sections.edit',$section), 'name' => "نعديل معلومات قسم المقابلات : $section->section_name"]],['title'=> 'مشاهدة معلومات قسم المقابلات']];
 @endphp
 
-@section('title', 'مشاهدة معلومات القسم')
+@section('title', 'مشاهدة معلومات قسم المقابلات')
 
 @section('content')
 
-@component('components.forms.formCard',['title' =>  sprintf('مشاهدة معلومات القسم : %s (#%s)',$section->section_name,$section->id)])
+@component('components.forms.formCard',['title' =>  sprintf('مشاهدة معلومات قسم المقابلات : %s (#%s)',$section->section_name,$section->id)])
 
 {!! Form::model($section,['route' => ['appointments.sections.update',$section],'method'=>'PUT' , 'onsubmit' => 'showLoader()'])  !!}
 
-<x-ui.divider>معلومات القسم</x-ui-divider>
+<x-ui.divider>معلومات قسم المقابلات</x-ui-divider>
 
 <div class="row mb-1">
     <div class="col-md">
-        <x-inputs.text.Input icon="file-text" label="اسم القسم" name="section_name" placeholder="ادخل اسم القسم" data-msg="'اسم القسم بشكل صحيح" />
+        <x-inputs.text.Input icon="file-text" label="اسم قسم المقابلات" name="section_name" placeholder="ادخل اسم قسم المقابلات" data-msg="'اسم قسم المقابلات بشكل صحيح" />
     </div>
 
     <div class="col-md">
