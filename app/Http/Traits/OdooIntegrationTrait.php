10414d45a7248d31e61cadcbb95dc2905f7929e4
AbdullahMohammedAbdelmoemen <abdullah.mohammed1997@outlook.com>
2022-12-21 20:27:30 +0200

    enhance code

--- app/Http/Traits/OdooIntegrationTrait.php
+++ app/Http/Traits/OdooIntegrationTrait.php
@@ -180,23 +180,21 @@ trait OdooIntegrationTrait
             curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 
             $response = curl_exec($curl);
-
             $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
             $response = json_decode($response);
             curl_close($curl); // Close the connection
 
             $msg = ($httpcode == 200 && isset($response->result))?$response->result->message:'';
 
-            if($httpcode == 200 && isset($response->result) && isset($response->result->success) && $response->result->success){
+            DB::transaction(function () use ($response,$msg,$payment_id,$httpcode){
+                DB::table('payment_attempts')->where("id",$payment_id)->update([
+                    "odoo_record_id" => ($httpcode == 200 && isset($response->result))?$response->result->ID:null,
+                    "odoo_sync_status" => ($httpcode == 200 && isset($response->result) && $response->result->success) ? 1 : 0,
+                    "odoo_message" => $msg
+                ]);
+            });
 
-                DB::transaction(function () use ($response,$msg,$payment_id){
-                    DB::table('payment_attempts')->where("id",$payment_id)->update([
-                        "odoo_record_id" => isset($response->result)?$response->result->ID:null,
-                        "odoo_sync_status" => (isset($response->result) && $response->result->success) ? 1 : 0,
-                        "odoo_message" => $msg
-                    ]);
-                });
+            if($httpcode == 200 && isset($response->result) && isset($response->result->success) && $response->result->success){
                 return redirect()->back()
                     ->with('alert-info', 'تم اضافه دفعه التعاقد في odoo بنجاح');
             }
