a16720a7843b3b1636228a9d306f7800d1aa947a
AbdullahMohammedAbdelmoemen <abdullah.mohammed1997@outlook.com>
2022-12-21 12:03:18 +0200

    make command

---
+++ app/Console/Commands/AddParentInOdooCommands.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Traits\OdooIntegrationTrait;
+use App\Models\guardian;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class AddParentInOdooCommands extends Command
+{
+    use OdooIntegrationTrait;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'install:AddParentInOdoo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $guardians = guardian::where("created_at",">=",Carbon::parse("01-08-2022 00:00:00"))->where("odoo_sync_status", 0)->get();
+
+
+        foreach ($guardians as $guardian){
+            $guardian->setOdooKeys($guardian);
+            $this->createParentInOdoo($guardian->getOdooKeys());
+        }
+
+        return Command::SUCCESS;
+    }
+}
